c61af166c7407cd41c5a2315aa5d26228defb6d9
Gabriel7576 <gabrielamaral7575@gmail.com>
2025-04-05 18:54:30 -0300

    refactor: alterar @RequestMapping no EmployeeController

--- src/main/java/com/business_management/controlle/EmployeeController.java
+++ src/main/java/com/business_management/controlle/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.business_management.controlle;
 
+import com.business_management.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.business_management.service.EmployeeService;
-
 @RestController
-@RequestMapping("/v1/employee")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
