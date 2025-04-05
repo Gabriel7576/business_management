d3c4e6c604e26793a809152915a848dcd56313f8
Gabriel7576 <gabrielamaral7575@gmail.com>
2025-04-05 18:49:17 -0300

    feat: implementar novas funcionalidades ao StoreController e StoreService

--- src/main/java/com/business_management/controlle/StoreController.java
+++ src/main/java/com/business_management/controlle/StoreController.java
@@ -1,16 +1,58 @@
 package com.business_management.controlle;
 
+import com.business_management.domain.Store;
+import com.business_management.dto.response.StoreCreationDTO;
+import com.business_management.dto.response.StoreUpdateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.business_management.service.StoreService;
 
+import java.net.URI;
+import java.util.List;
+
 @RestController
-@RequestMapping("/v1/store")
+@RequestMapping("/store")
 public class StoreController {
 
     @Autowired
-    private StoreService storeService;
+    private StoreService service;
+
+    @PostMapping
+    public ResponseEntity<Store> saveStore(@RequestBody StoreCreationDTO creationDTO) {
+        var store_id = service.saveStore(creationDTO);
+
+        return ResponseEntity.created(URI.create("/v1/store/" + store_id.toString())).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Store> getStoreById(@PathVariable Long id) {
+        var store = service.findById(id);
+
+        return store.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Store>> findAll() {
+        List<Store> list = service.findAll();
+
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateStoreById(@PathVariable("id") Long id, @RequestBody StoreUpdateDTO updateDTO) {
+        service.updateStore(id, updateDTO);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable("id") Long id) {
+        service.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/business_management/service/StoreService.java
+++ src/main/java/com/business_management/service/StoreService.java
@@ -1,14 +1,72 @@
 package com.business_management.service;
 
+import com.business_management.domain.Store;
+import com.business_management.dto.response.StoreCreationDTO;
+import com.business_management.dto.response.StoreUpdateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.business_management.repository.StoreRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StoreService {
 
     @Autowired
-    private StoreRepository storeRepository;
+    private StoreRepository repository;
+
+    public Long saveStore(StoreCreationDTO creationDTO) {
+
+        Store entity = new Store(creationDTO.name(), creationDTO.address(), creationDTO.phone(), creationDTO.email());
+
+        Store storeSaved = repository.save(entity);
+
+        return storeSaved.getStore_id();
+    }
+
+    public Optional<Store> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<Store> findAll() {
+        return repository.findAll();
+    }
+
+    public void updateStore(Long id, StoreUpdateDTO updateDTO) {
+
+        Optional<Store> storeEntity = findById(id);
+
+        if (storeEntity.isPresent()) {
+            Store store = storeEntity.get();
+
+            if (updateDTO.name() != null)
+                store.setName(updateDTO.name());
+
+            if (updateDTO.address() != null)
+                store.setAddress(updateDTO.address());
+
+            if (updateDTO.phone() != null)
+                store.setPhone(updateDTO.phone());
+
+            if (updateDTO.email() != null)
+                store.setEmail(updateDTO.email());
+
+            repository.save(store);
+        }
+
+    }
+
+    public void deleteById(Long id) {
+
+        Optional<Store> storeEntity = findById(id);
+
+        if (storeEntity.isPresent()) {
+            repository.deleteById(id);
+        }
+
+    }
+
 
 }
