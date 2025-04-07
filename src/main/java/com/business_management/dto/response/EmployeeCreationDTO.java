7a37d4d4c5afa4a88084d72453c1830dba23494a
Gabriel7576 <gabrielamaral7575@gmail.com>
2025-04-07 18:59:10 -0300

    feat: criar EmployeeUpdateDTO e EmployeeCreationDTO

---
+++ src/main/java/com/business_management/dto/response/EmployeeCreationDTO.java
@@ -0,0 +1,4 @@
+package com.business_management.dto.response;
+
+public record EmployeeCreationDTO(String name, String email, String phone, Long store_id) {
+}
---
+++ src/main/java/com/business_management/dto/response/EmployeeUpdateDTO.java
@@ -0,0 +1,4 @@
+package com.business_management.dto.response;
+
+public record EmployeeUpdateDTO(String name, String email, String phone) {
+}
